98e5634adb0bd80931a127603d233c35c46b8a09
Leo <leopaul.dewaele@gmail.com>
2024-05-07 16:12:42 +0200

    continuation

--- structure.py
+++ structure.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 
 class Mur:
     """Créé un mur"""
@@ -10,31 +10,44 @@ class Mur:
 
 
 
-class Vehicule:
-    """Créé un vhécule"""
+class Moto:
+    """Créé une moto"""
     
-    def __init__(self, nb_roue, pos_roue, centre_gravite, masse, largeur, longueur) -> None:
+    def __init__(self, rayon_roue, centre_gravite, masse, largeur, empattement, vitesse, braquage) -> None:
         """Initialise une voiture avec son repère local
                     y
                     ^
                     |
                     |
-             long --⟥---------⟤ (<- position roues - aux coins par ex)
-                    |          |
-                    |          |
-                    |     +    |
-                    |    Cg    |
-                    |          |
-                    ⟥---------⟤----------> x
-                 (0,0)        larg
+             empat  |     |  <- Roues 
+                    |     |     
+                    |     |     
+                    |     +    
+                    |     | Cg    
+                    |     |     
+                    |-----|-------------> x
+                 (0,0)   larg
         """
         
-        self.nb_roue = nb_roue                 # Nombre de contact
-        self.pos_roue = pos_roue               # Position des roues dans le repère local
-        self.centre_gravite = centre_gravite   # Position du centre de gravtité dans le repère local
-        self.masse = masse                     # Masse du véhicule
-        self.largeur = largeur                 # Largeur du véhicule
-        self.longueur = longueur               # Longueur du véhicule
+        self.rayon_roue = rayon_roue                # Rayon des roues
+        self.braquage_avant = braquage              # Braquage
+        self.centre_gravite = centre_gravite        # Position du centre de gravtité dans le repère local
+        self.masse = masse                          # Masse du véhicule
+        self.largeur = largeur                      # Largeur du véhicule
+        self.empattement = empattement              # Longueur du véhicule
+        self.vitesse_G = [vitesse,np.tan(braquage) * centre_gravite[1] / empattement]  # Vitesse du centre de gravité [v,theta]
 
-        #ddlkdkodlkelkf
-        #eeuyrtr
+    def verification_glissement(self) -> bool:
+        """Vérifie qu'il n'y ai pas de glissement de la roue avant pour f (frottement roue sol)"""
+        f = 1
+        force_centrifuge = self.masse * self.vitesse_G[0]**2 / np.sin(self.braquage_avant) * self.empattement
+        poids = self.masse * 9.81
+        force_normale_sol = (self.empattement - self.centre_gravite[1])/self.empattement * poids          # Force normale sur la roue avant
+
+        force_frottement = f * force_normale_sol
+
+        return force_centrifuge > force_frottement
+
+        
+        
+
