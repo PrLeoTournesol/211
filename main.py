8c78cf88c0972f4eca693c1850ddac24a1511611
Leo <leopaul.dewaele@gmail.com>
2024-05-14 14:31:36 +0200

    test trajectoire

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 moto = Moto(0.3, 0.5, 400, 2, 5)
 x = []
 y = []
+x2 = []
+y2 = []
 
 test = []
     
@@ -20,15 +22,19 @@ while current_time < 10:
 
     moto.acceleration = 0
 
-    if current_time > 1 and current_time < 5:
-        moto.inclinaison = np.pi/3 * (current_time-1) * 0.1
-    elif current_time > 5 and current_time < 8:
-        moto.inclinaison = np.pi/3 * 0.4 - np.pi/3 * 0.4/3 * (current_time-5)
-    elif current_time > 8 : 
-        moto.inclinaison = 0
+    #if current_time > 1 and current_time < 5:
+    #    moto.inclinaison = np.pi/3 * (current_time-1) * 0.1
+    #elif current_time > 5 and current_time < 8:
+    #    moto.inclinaison = np.pi/3 * 0.4 - np.pi/3 * 0.4/3 * (current_time-5)
+    #elif current_time > 8 : 
+    #    moto.inclinaison = 0
     # print('vit : ', moto.vitesse)
     # print('braq : ', moto.braquage_avant)
     # print('dt : ', dt)
+    
+    moto.inclinaison = np.exp(-current_time)*np.cos(current_time-1)*current_time
+    
+    
     # print(' ')
 
     moto.update_inclinaison(dt)
@@ -39,8 +45,12 @@ while current_time < 10:
     moto.update_angle(dt)
     moto.calcul_rotation_matrice()
     pos = moto.changement_repere(moto.position_moto)
+    
     x.append(pos[0])
     y.append(pos[1])
+    if current_time > 5 and current_time < 8:
+        x2.append(pos[0])
+        y2.append(pos[1])
 
     #print(moto.rayon_courbure)
 
@@ -50,4 +60,5 @@ while current_time < 10:
 plt.figure()
 #plt.plot(range(len(test)), test)
 plt.plot(x, y)
+plt.plot(x2, y2,'r')
 plt.show()
